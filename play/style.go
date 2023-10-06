857e49d5d41936d4b606426e60923c672c782194
Nikita Rusin <rusinikit@gmail.com>
2023-10-06 17:36:24 +0200

    Move code copy advise to help line

--- play/style.go
+++ play/style.go
@@ -24,17 +24,19 @@ var (
 				BorderStyle(lipgloss.DoubleBorder()).
 				BorderForeground(lipgloss.Color("170"))
 	errorStyle       = lipgloss.NewStyle().Foreground(lipgloss.Color("196")).Margin(1)
-	questionTagStyle = lipgloss.NewStyle().Padding(0, 1).Margin(1).Background(lipgloss.Color("62")).Foreground(lipgloss.Color("230"))
+	tagStyle         = lipgloss.NewStyle().Padding(0, 1).Background(lipgloss.Color("62")).Foreground(lipgloss.Color("230"))
+	questionTagStyle = tagStyle.Copy().Margin(1)
 	questionEndStyle = lipgloss.NewStyle().Margin(1).Foreground(lipgloss.Color("34"))
 
 	listStyle         = lipgloss.NewStyle().MarginTop(1)
 	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
 	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
 
-	learningTitle        = lipgloss.NewStyle().Bold(true).Italic(true)
-	learningAdviseText   = lipgloss.NewStyle().Padding(1, 0, 1, 4)
-	marginH1             = lipgloss.NewStyle().Margin(0, 1)
-	helpStyle            = lipgloss.NewStyle().Margin(1).MarginBottom(0)
+	learningTitle      = lipgloss.NewStyle().Bold(true).Italic(true)
+	learningAdviseText = lipgloss.NewStyle().Padding(1, 0, 1, 4)
+	marginH1           = lipgloss.NewStyle().Margin(0, 1)
+	helpStyle          = lipgloss.NewStyle().Margin(1).MarginBottom(0).
+				Foreground(lipgloss.AdaptiveColor{Light: "#B2B2B2", Dark: "#4A4A4A"})
 	learningLink         = lipgloss.NewStyle().BorderStyle(lipgloss.RoundedBorder())
 	learningLinkSelected = learningLink.Copy().
 				Foreground(lipgloss.Color("170")).
